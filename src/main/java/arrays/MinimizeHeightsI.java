42db7b77337f8ad7803207f07abafa4894db4a4b
rohan1.gupta@paytm <rohan1.gupta@paytm.com>
2022-04-24 12:18:23 +0530

    Min heights 1

---
+++ src/main/java/arrays/MinimizeHeightsI.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MinimizeHeightsI {
+    public static void main(String[] args) {
+
+    }
+
+    int getMinDiff(int[] arr, int n, int k) {
+        if(n==1){
+            return 0;
+        }
+        Arrays.sort(arr);
+        int diff = arr[n-1] - arr[0];
+        int minimum,maximum;
+        for(int i=1;i<n;i++){
+            maximum = Math.max(arr[i-1]+k, arr[n-1]-k);
+            minimum = Math.min(arr[0]+k, arr[i]-k);
+            diff = Math.min(diff,maximum-minimum);
+        }
+        return diff;
+    }
+}
